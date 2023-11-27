0c2b845f9a2798cfffa603e0b12ee2de0f9ea6bb
Rafael Jovel <rafael.jovel@mh.gob.sv>
2023-11-26 18:24:14 -0600

    express api

---
+++ app.js
@@ -0,0 +1,18 @@
+// Importa el módulo express
+const express = require('express');
+
+// Crea una aplicación de Express
+const app = express();
+
+// Define una ruta que responda con tu nombre
+app.get('/', (req, res) => {
+    res.send('¡Hola! Mi nombre es Rafael Albino Jovel Alfaro');
+});
+
+// Especifica el puerto en el que escuchará el servidor
+const puerto = 3000;
+
+// Hace que el servidor escuche las solicitudes en el puerto especificado
+app.listen(puerto, () => {
+    console.log(`Servidor escuchando en http://localhost:${puerto}`);
+});
